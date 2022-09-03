cf92505bef514db3b9cfde0854bf4efb5183655b
Matthew chen <Matgenius04@users.noreply.github.com>
2022-09-03 01:59:14 -0500

    I gave up on serverless

--- process-photos.js
+++ process-photos.js
@@ -1,3 +1,4 @@
-import {readdir} from "fs/promises";
+import {readdir, writeFile} from "fs/promises";
 
-console.log(await readdir('static/club-photos'));
+await writeFile("./src/lib/club-photos.json",JSON.stringify(await readdir('static/club-photos')));
+console.log("image path rendered","\n","--------------------","\n");
--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,11 +1,10 @@
 <script>
-	  /** @type {import('./$types').PageData} */  
-	  export let data;
+	import images from "$lib/images.json"
 </script>
 
 <main>
 	<h1>Photos</h1>
-	{#each Object.values(data) as image, i}
+	{#each images as image, i}
 		<img src={"club-photos/"+ image} alt="club-photo-{i}"/>
 	{/each}
 </main>
