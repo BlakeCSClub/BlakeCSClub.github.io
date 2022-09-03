46438875dc0342a4e064fbb6367474d5bb844dc7
Matthew chen <Matgenius04@users.noreply.github.com>
2022-09-03 01:15:39 -0500

    this is obscene

--- src/routes/photos/+page.server.js
+++ src/routes/photos/+page.server.js
@@ -2,19 +2,38 @@ import { error } from '@sveltejs/kit';
 import { readdir } from 'fs/promises';
 import { join } from 'path';
 import { cwd } from 'process';
-import { promisify } from 'util';
  
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
   // console.log(cwd());
   // console.log(await (promisify(glob))(cwd()+'/**/*'))
-  console.log(cwd(),await readdir(join(cwd(),".svelte-kit","output","client","club-photos")));
+  console.log(cwd(),await readdir(join(cwd())));
+  try {
+    // const images = await readdir(join(cwd(),"static","club-photos"));
+    const images = await readdir(join(cwd(),".svelte-kit","output","client","club-photos"));
 
-  // const images = await readdir(join(cwd(),"static","club-photos"));
-  const images = await readdir(join(cwd(),".svelte-kit","output","client","club-photos"));
-
-  if (images) {
-    return images;
+    // if (images) {
+    //   return images;
+    // }
+  } catch {
+    console.log("stuff's still broken")
+  }
+  try {
+    console.log(await readdir(join(cwd(),".svelte-kit","output","client","club-photos")))
+  } catch {
+    try {
+      console.log(await readdir(join(cwd(),".svelte-kit","output","client")))
+    } catch {
+      try {
+        console.log(await readdir(join(cwd(),".svelte-kit","output")))
+      } catch {
+        try {
+          console.log(await readdir(join(cwd(),".svelte-kit")))
+        } catch {
+          console.log("this is stupid")
+        }
+      }
+    }
   }
  
   throw error(404, "Not Found");
